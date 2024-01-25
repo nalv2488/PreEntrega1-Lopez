de129ed06913983f40b3b97ca5062332b35cfd66
nalv2488 <lopezv.nicolas@gmail.com>
2024-01-24 23:55:37 -0300

    Estilo Parte 1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import "./itemlistcontainer.css"
 
-function ItemListContainer({saludo}) {
-  return (
-    <>
-    
-    <div className='div-saludo'>
-    {saludo}
-    </div>
-    
-    </>
-  )
+function ItemListContainer({ saludo }) {
+    return (
+        <>
+
+            <div className='div-saludo'>
+                {saludo}
+            </div>
+
+        </>
+    )
 }
 
 export default ItemListContainer
